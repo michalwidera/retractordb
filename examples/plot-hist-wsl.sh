6b79c6b8a4a83a75c4026e3712747947f2a26d75
Michal Widera <michal@widera.com.pl>
2020-01-06 01:39:06 +0100

    visualisation

---
+++ examples/plot-hist-wsl.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+trap control_c SIGINT
+
+control_c()
+{
+    xqry -k
+    pkill plothistgram
+    stty sane
+}
+
+echo "Type ctrl+c to stop."
+
+if ! xcompiler -q query-histogram.txt ; then exit 1 ; fi
+
+nohup xabracadabra &
+
+sleep 4
+export DISPLAY=:0
+xqry -s countHistorgram | plothistgram.py 0 25 "first-argument:red;second-argument:blue" --sleep 0.5 --term wxt | gnuplot 2>/dev/null
