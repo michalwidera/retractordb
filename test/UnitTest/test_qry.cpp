a33ab2a3397f300d5141986647623f78f48c78b4
Michal Widera <michal@widera.com.pl>
2021-10-08 23:54:24 +0200

    Issue #21 - Implement conan package manager
    This change introduce conan package manager for RetractorDB project
    Due this artifacts like gtest or boost could starts to be manageable

--- test/UnitTest/test_qry.cpp
+++ test/UnitTest/test_qry.cpp
@@ -1,6 +1,4 @@
-#define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE Hello
-#include <boost/test/unit_test.hpp>
+#include <gtest/gtest.h>
 
 #include <boost/system/error_code.hpp>
 #include "qry/qry.hpp"
@@ -9,7 +7,7 @@ bool check_hello_function() {
     return ( hello() == boost::system::errc::protocol_error );
 }
 
-BOOST_AUTO_TEST_CASE( test_hello )
+TEST( xqry , test_hello )
 {
-    BOOST_CHECK( check_hello_function() == true );
+    ASSERT_TRUE( check_hello_function() );
 }
