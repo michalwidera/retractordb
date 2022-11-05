1a2339d5a3c4463ae5927647902a4de196dc228f
Michal Widera <michal@widera.com.pl>
2022-11-05 23:40:58 +0100

    Get rid of comments in collapse mode

--- src/retractor/dataModel.h
+++ src/retractor/dataModel.h
@@ -24,27 +24,35 @@ struct dataModel {
   std::map<std::string, rdb::DataStorageAccessor<std::byte>> dataStreamSet;
   std::map<std::string, rdb::Descriptor> internalDescriptorSet;
 
-  // Mapa zawierająca informacje powiązane z wewnętrznym deskryptorem
-  // domyślinie - każdy rekord jest świeży i niegotowy == false - Not Ready To Check In
-  // jeśli jest wyliczony i gotowy do zapisu to == true - Ready To Check In
-  std::map<std::string, std::map<std::string, bool>>
-      publicDescriptorStatusSet;
+  /**
+   * Mapa zawierająca informacje powiązane z wewnętrznym deskryptorem
+   * domyślinie - każdy rekord jest świeży i niegotowy == false - Not Ready To
+   * Check In jeśli jest wyliczony i gotowy do zapisu to == true - Ready To
+   * Check In
+   */
+  std::map<std::string, std::map<std::string, bool>> publicDescriptorStatusSet;
 
-  // Dane z wewnętrznego schematu służą do wyznaczenia publicznego obrazu strumienia
-  // dlatego tylko występue getInternal - bez setInternal
+  /**
+   * Dane z wewnętrznego schematu służą do wyznaczenia publicznego obrazu
+   * strumienia dlatego tylko występue getInternal - bez setInternal
+   */
   number getInternal(std::string streamName, int position);
 
-  // Dane występujące w publicznym schemacie są pobierane przez zewnętrzne zapytania
-  // i zapisywane jednokrotnie przez wewnętrzne.
-  // czyli wiele getPublic z zewnątrz i tylko jedno wywołanie setPublic z wewnętrznego
-  // wyliczenia
-  // setPublic powinno ustawić w publicDescriptorStatusSet wartość true - tzn. Ready To Check In
+  /**
+   *  Dane występujące w publicznym schemacie są pobierane przez zewnętrzne
+   * zapytania i zapisywane jednokrotnie przez wewnętrzne. czyli wiele getPublic
+   * z zewnątrz i tylko jedno wywołanie setPublic z wewnętrznego wyliczenia
+   *  setPublic powinno ustawić w publicDescriptorStatusSet wartość true - tzn.
+   * Ready To Check In
+   */
   number getPublic(std::string streamName, int position);
   void setPublic(std::string streamName, int position, number value);
 
-  // zapis danych do strumienia powinnien się udać tylko jeśli
-  // wszystkie elementy publicznego deskryptora zostały zatwierdzone 
-  // tzn. są w stanie Ready To Check In
+  /**
+   * zapis danych do strumienia powinnien się udać tylko jeśli
+   * wszystkie elementy publicznego deskryptora zostały zatwierdzone
+   * tzn. są w stanie Ready To Check In
+   */
   bool flushDataToStream(std::string streamName);
 
   void registerStream(std::string streamName,
