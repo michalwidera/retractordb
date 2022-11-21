ec16a6ac646bd394ad71c715a48fa73330d4d6f9
Michal Widera <michal@widera.com.pl>
2022-11-21 21:08:56 +0100

    Add payloadaccessor to dataModel

--- src/retractor/dataModel.cpp
+++ src/retractor/dataModel.cpp
@@ -2,10 +2,14 @@
 
 #include <rdb/payloadacc.h>  // rdb::payLoadAccessor
 
+enum { noHexFormat = false, HexFormat = true };
+
 streamInstance::streamInstance(const std::string file) {
   // Ta sekwencja otworzy plik danych i deskrypor
   uPtr_storage.reset(new rdb::DataStorageAccessor(file));
   uPtr_payload.reset(new std::byte[uPtr_storage->getDescriptor().GetSize()]);
+  uPtr_plAcces.reset(new rdb::payLoadAccessor(externalDataDescriptor,
+                                              uPtr_payload.get(), noHexFormat));
   // internalData.resize(internalDataDescriptor.size());
   // externalData.resize(externalDataDescriptor.size());
 };
