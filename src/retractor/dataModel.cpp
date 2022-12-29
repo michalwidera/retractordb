9e25581d40952d915ec927740f2adc154f2b6418
Michal Widera <michal@widera.com.pl>
2022-12-29 23:12:12 +0100

    Refactor dataModel

--- src/retractor/dataModel.cpp
+++ src/retractor/dataModel.cpp
@@ -1,14 +1,24 @@
 #include "dataModel.h"
 
-#include <assert.h>
-#include <rdb/payloadacc.h>  // rdb::payLoadAccessor
-
-#include "QStruct.h"
+#include "QStruct.h" // coreInstance
 
 extern "C" qTree coreInstance;
 
+std::any streamComposite::get(int position) {
+  return accessor->getItem(position);
+};
+
+void streamComposite::set(int position, std::any value) {
+  accessor->setItem(position, value);
+};
+
+streamComposite::streamComposite(rdb::Descriptor descriptor) {
+  payload.reset(new std::byte[descriptor.getSizeInBytes()]);
+  accessor.reset(
+      new rdb::payLoadAccessor(descriptor, payload.get(), noHexFormat));
+};
+
 streamInstance::streamInstance(const std::string file) {
-  // Ta sekwencja otworzy plik danych i deskrypor
   storage.reset(new rdb::DataStorageAccessor(file));
   publicSchema.reset(new streamComposite(storage->getDescriptor()));
   internalSchema.reset(
