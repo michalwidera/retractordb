559c268bcfa44395770f271784cd3a2bb42e6a56
Michal Widera <michal@widera.com.pl>
2022-12-29 23:12:12 +0100

    Refactor dataModel

--- src/retractor/dataModel.h
+++ src/retractor/dataModel.h
@@ -2,45 +2,25 @@
 
 #include <rdb/desc.h>   // rdb::Descriptor
 #include <rdb/dsacc.h>  // rdb::DataStorageAccessor
-#include <rdb/payloadacc.h>
-#include <spdlog/spdlog.h>
+#include <rdb/payloadacc.h>  // rdb::payLoadAccessor
 
 #include <any>
-#include <cassert>
 #include <map>
 #include <memory>  // unique_ptr
 #include <string>
 #include <vector>
 
-#include "QStruct.h"  // number
-
 struct streamComposite {
   enum { noHexFormat = false, HexFormat = true };
   std::unique_ptr<std::byte[]> payload;
   std::unique_ptr<rdb::payLoadAccessor<std::byte>> accessor;
-  std::any get(int position) { return accessor->getItem(position); };
-  void set(int position, std::any value) {
-    accessor->setItem(position, value);
-  };
-  streamComposite(rdb::Descriptor descriptor) {
-    payload.reset(new std::byte[descriptor.getSizeInBytes()]);
-    accessor.reset(
-        new rdb::payLoadAccessor(descriptor, payload.get(), noHexFormat));
-  };
+
+  std::any get(int position);
+  void set(int position, std::any value);
+
+  streamComposite(rdb::Descriptor descriptor);
 };
 
-/**
- * Ta klasa ma za zadnie przedstawić warstwę abstrakcji danych.
- * Problem polegał na tym że poszególne strumienie i zapytania muszą inaczej
- * interpretować nazwy kolumn.
- * I tak select a[0],a[2] stream a from b+c
- * zakładając że strumienie b i c tworzą co najmnie trzelementowy zbiór to
- * to zapytanie tworzy strumień a o elementach a[0],a[1] ale z elementów sumy
- * schematów strumieni b i c z klauzuli select (wewnątrz zapytania) widać 0,1,2
- * a z zewnątrz (z innych zapytań) tylko 0 i 1
- *
- * TODO: Translate to english afterall - before merge to Master
- */
 struct streamInstance {
   std::unique_ptr<rdb::DataStorageAccessor<std::byte>> storage;
 
