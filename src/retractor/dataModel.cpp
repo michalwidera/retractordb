8e893786000178dcb066fa662179d5f8cf5b7296
Michal Widera <michal@widera.com.pl>
2023-01-03 12:37:54 +0100

    line formating in log

--- src/retractor/dataModel.cpp
+++ src/retractor/dataModel.cpp
@@ -1,12 +1,16 @@
 #include "dataModel.h"
-#include "spdlog/spdlog.h"
+
 #include <cassert>
+#include <regex>
 
 #include "QStruct.h"  // coreInstance
+#include "spdlog/spdlog.h"
 
 // ctest -R '^unittest-test-schema'
 
 extern "C" qTree coreInstance;
+
+std::string removeCRLF(std::string input) { return std::regex_replace(input, std::regex("\\r\\n|\\r|\\n"), ""); }
 
 std::any streamComposite::get(int position) { return accessor->getItem(position); };
 
@@ -17,22 +21,49 @@ streamComposite::streamComposite(rdb::Descriptor descriptor) {
   accessor.reset(new rdb::payLoadAccessor(descriptor, payload.get(), noHexFormat));
 };
 
+streamInstance::streamInstance(          //
+    const std::string file,              //
+    const rdb::Descriptor descInternal,  //
+    const rdb::Descriptor descExternal   //
+) {
+  storage.reset(new rdb::DataStorageAccessor(file));
+  storage->createDescriptor(descExternal);
+  external.reset(new streamComposite(descExternal));
+  internal.reset(new streamComposite(descInternal));
+
+  {
+    std::stringstream strStream;
+    strStream << storage->getDescriptor();
+    SPDLOG_INFO("storage descriptor: {}", removeCRLF(strStream.str()));
+  }
+  {
+    std::stringstream strStream;
+    strStream << external->accessor->getDescriptor();
+    SPDLOG_INFO("external descriptor: {}", removeCRLF(strStream.str()));
+  }
+  {
+    std::stringstream strStream;
+    strStream << internal->accessor->getDescriptor();
+    SPDLOG_INFO("internal descriptor: {}", removeCRLF(strStream.str()));
+  }
+};
+
 streamInstance::streamInstance(const std::string file) {
   storage.reset(new rdb::DataStorageAccessor(file));
   external.reset(new streamComposite(storage->getDescriptor()));
-  //assert(storage->getDescriptor() == coreInstance[file].descriptorExpression());
+  // assert(storage->getDescriptor() == coreInstance[file].descriptorExpression());
 
   std::stringstream strStream;
   strStream << storage->getDescriptor();
-  SPDLOG_INFO("storage descriptor: {}", strStream.str());
+  SPDLOG_INFO("storage descriptor: {}", removeCRLF(strStream.str()));
 
-  //strStream.clear();
-  //strStream << coreInstance[file].descriptorFrom();
-  //SPDLOG_INFO("descriptorFrom descriptor: {}", strStream.str());
+  // strStream.clear();
+  // strStream << coreInstance[file].descriptorFrom();
+  // SPDLOG_INFO("descriptorFrom descriptor: {}", strStream.str());
 
-  //strStream.clear();
-  //strStream << coreInstance[file].descriptorExpression();
-  //SPDLOG_INFO("descriptorExpression descriptor: {}", strStream.str());
+  // strStream.clear();
+  // strStream << coreInstance[file].descriptorExpression();
+  // SPDLOG_INFO("descriptorExpression descriptor: {}", strStream.str());
 
-  //internal.reset(new streamComposite(coreInstance[file].descriptorFrom()));
+  // internal.reset(new streamComposite(coreInstance[file].descriptorFrom()));
 };
--- test/UnitTest/test_schema.cpp
+++ test/UnitTest/test_schema.cpp
@@ -14,6 +14,8 @@
 
 extern "C" qTree coreInstance;
 
+// ctest -R unittest-test-schema
+
 TEST(xschema, check_test1) {
   /*
   query qry;
@@ -23,8 +25,18 @@ TEST(xschema, check_test1) {
   qry.lSchema.push_back(field("*", lSchema, rdb::BAD, "*"));
   coreInstance.push_back(qry);
   */
+  auto dataDescriptorInternal{
+      rdb::Descriptor("A[1]", rdb::INTEGER) |  //
+      rdb::Descriptor("A[2]", rdb::INTEGER) |  //
+      rdb::Descriptor("A[3]", rdb::INTEGER)    //
+  };
 
-  streamInstance data{"file_1.dat"};
+  auto dataDescriptorExternal{
+      rdb::Descriptor("A[1]", rdb::INTEGER) |  //
+      rdb::Descriptor("A[2]", rdb::INTEGER)    //
+  };
+
+  streamInstance data{"file_A.dat", dataDescriptorInternal, dataDescriptorExternal};
 
   ASSERT_TRUE(true);
 };
