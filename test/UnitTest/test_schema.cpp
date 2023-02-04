b5b16df97c44ff93220ba675c52dbceebfaeca52
Michal Widera <michal@widera.com.pl>
2023-02-04 23:22:32 +0100

    UT

--- test/UnitTest/test_schema.cpp
+++ test/UnitTest/test_schema.cpp
@@ -13,7 +13,17 @@
 #include "retractor/dataModel.h"
 #include "spdlog/spdlog.h"
 
+extern std::string parser(std::string sInputFile);
+
 extern "C" qTree coreInstance;
+
+/*
+DECLARE a INTEGER, b BYTE STREAM core0, 1 FILE '/dev/urandom'
+DECLARE c INTEGER,d BYTE STREAM core1, 0.5 FILE '/dev/urandom'
+SELECT core0[0],core0[1] STREAM str1 FROM core0#core1
+SELECT * STREAM test1 FROM core@(-1,10)
+SELECT * STREAM test2 FROM core@(1,10)
+*/
 
 // ctest -R unittest-test-schema
 
@@ -27,6 +37,9 @@ TEST(xschema, check_test1) {
   coreInstance.push_back(qry);
   */
 
+  coreInstance.clear();
+  auto compiled = parser("ut_example.rql") == "OK";
+
   auto dataInternalDesciptor{
       rdb::Descriptor("A[1]", rdb::INTEGER) |  //
       rdb::Descriptor("A[2]", rdb::INTEGER) |  //
@@ -38,16 +51,37 @@ TEST(xschema, check_test1) {
       rdb::Descriptor("A[2]", rdb::INTEGER)    //
   };
 
-  dataInstance data{"file_A.dat", dataStorageDescriptor, dataInternalDesciptor};
+  {
+    dataInstance data{"file_A.dat", dataStorageDescriptor, dataInternalDesciptor};
 
-  data.internalPayload->setItem(0, 123);
-  data.internalPayload->setItem(1, 345);
+    data.internalPayload->setItem(0, 123);
+    data.internalPayload->setItem(1, 345);
 
-  auto v1 = data.internalPayload->getItem(0);
+    auto v1 = data.internalPayload->getItem(0);
 
-  data.storagePayload->setItem(0, 234);
-  data.storagePayload->setItem(1, 456);
-  data.storage->write();
+    data.storagePayload->setItem(0, 234);
+    data.storagePayload->setItem(1, 456);
+    data.storage->write();
+  }
+
+  {
+    dataInstance data{"file_A.dat", dataStorageDescriptor, dataInternalDesciptor};
+
+    data.internalPayload->setItem(0, 123);
+    data.internalPayload->setItem(1, 345);
+
+    auto v1 = data.internalPayload->getItem(0);
+
+    data.storagePayload->setItem(0, 234);
+    data.storagePayload->setItem(1, 456);
+    data.storage->write();
+  }
+
+  // for (auto q : coreInstance) {
+  //  SPDLOG_INFO("coreInstance[] {}, {}", q.id, q.filename);
+  //}
+
+  dataInstance data(coreInstance["str1"]);
 
   ASSERT_TRUE(true);
 };
