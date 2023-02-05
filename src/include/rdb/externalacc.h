285612acca0930e5c2c7eaeecec8e2295f77cb77
Michal Widera <michal@widera.com.pl>
2023-02-05 20:27:17 +0100

    Filling externalaccessor

--- src/include/rdb/externalacc.h
+++ src/include/rdb/externalacc.h
@@ -1,20 +1,27 @@
 #ifndef EXTERMALACC_RDB_INCLUDE_DACC_H_
 #define EXTERMALACC_RDB_INCLUDE_DACC_H_
 
-#include <string>
 #include <cstddef>  // std::byte
+#include <string>
 
 #include "descriptor.h"
+#include "payload.h"
 
 namespace rdb {
 class externalAccessor {
+  Descriptor descriptor;
   std::string filename;
   std::byte *payloadPtr;
+
  public:
   externalAccessor() = delete;
   externalAccessor(std::string fileName);
 
   void createDescriptor(const Descriptor descriptor);
+
+  void attachPayloadPtr(std::byte *payloadPtrVal);
+  void attachPayload(rdb::payload &payloadRef);
+
   bool read();
 
   Descriptor &getDescriptor();
