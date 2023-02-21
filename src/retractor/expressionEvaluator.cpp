9de81a2db747791ff35785cbe9ec679142af9bfc
Michal Widera <michal@widera.com.pl>
2023-02-21 13:26:33 +0100

    variant intro stack computation

--- src/retractor/expressionEvaluator.cpp
+++ src/retractor/expressionEvaluator.cpp
@@ -1,20 +1,40 @@
 #include "expressionEvaluator.h"
 
+#include <spdlog/spdlog.h>
+
 expressionEvaluator::expressionEvaluator(/* args */) {}
 
 expressionEvaluator::~expressionEvaluator() {}
 
-std::any add(std::any a, std::any b) { return 0; };
-std::any sub(std::any a, std::any b) { return 0; };
-std::any mul(std::any a, std::any b) { return 0; };
-std::any div(std::any a, std::any b) { return 0; };
-std::any neg(std::any a) { return 0; };
+template <class V>
+std::type_info const& var_type(V const& v) {
+  return std::visit([](auto&& x) -> decltype(auto) { return typeid(x); }, v);
+}
 
-std::any expressionEvaluator::eval(std::list<token> program) {
-  std::any retval;
+// This function will return two data that types are common and math-able
+std::pair<variant_t, variant_t> normalize(variant_t a, variant_t b) {
+  //SPDLOG_INFO("types {} {}", var_type(a), var_type(b));
 
-  std::stack<std::any> rStack;
-  std::any a, b;
+  if (var_type(a) == var_type(b)) return std::pair<variant_t, variant_t>(a, b);
+
+  return std::pair<variant_t, variant_t>(std::monostate{}, std::monostate{});
+}
+
+variant_t add(const variant_t &aParam, const variant_t &bParam) {
+  auto [a, b]{normalize(aParam, aParam)};
+  return 0;
+};
+
+variant_t sub(variant_t a, variant_t b) { return 0; };
+variant_t mul(variant_t a, variant_t b) { return 0; };
+variant_t div(variant_t a, variant_t b) { return 0; };
+variant_t neg(variant_t a) { return 0; };
+
+variant_t expressionEvaluator::eval(std::list<token> program) {
+  variant_t retval;
+
+  std::stack<variant_t> rStack;
+  variant_t a, b;
 
   for (auto tk : program) {
     switch (tk.getCommandID()) {
