3ce1ace8e0e514088bb7916c7c3ddbab388e1a07
Michal Widera <michal@widera.com.pl>
2023-02-22 21:07:48 +0100

    work

--- src/retractor/expressionEvaluator.h
+++ src/retractor/expressionEvaluator.h
@@ -1,20 +1,26 @@
 #pragma once
 
 #include <any>
+#include <boost/rational.hpp>
 #include <stack>
+#include <string>
 #include <variant>
 
-#include "QStruct.h"
+#include "QStruct.h"  // token
 
-typedef std::variant<std::monostate,        //
-                     uint8_t,               //
-                     int,                   //
-                     long,                  //
-                     unsigned,              //
-                     float,                 //
-                     double,                //
-                     std::string,           //
-                     boost::rational<int>>  //
+// enumDecl.h
+
+// sequence of types here is significatn
+// it is used to promote in math operations.
+typedef std::variant<std::monostate,        // BAD
+                     uint8_t,               // BYTE
+                     int,                   // INTEGER
+                     unsigned,              // UINT
+                     boost::rational<int>,  // RATIONAL
+                     float,                 // FLOAT
+                     double,                // DOUBLE
+                     std::string            // STRING
+                     >
     variant_t;
 
 class expressionEvaluator {
