478bc52d14bc34af09c31fdacf2bbdfaf43862ac
Michal Widera <michal@widera.com.pl>
2023-03-27 11:37:40 +0200

    Test was passing but failure was occured

--- test/Regression/Data/test-all-operators.sh
+++ test/Regression/Data/test-all-operators.sh
@@ -2,13 +2,13 @@
 
 if [ "$1" != "" ]; then
 
-if ! xretractor $1 -c; then exit ; fi
+if ! xretractor $1 -c; then exit 1 ; fi
 
 xretractor $1 -m 8000 &
 
 sleep 5
 xqry -d
-xqry -s str3 -m 5 
+xqry -s str3 -m 5
 xqry -s str2 -m 5
 xqry -l
 xqry -k
