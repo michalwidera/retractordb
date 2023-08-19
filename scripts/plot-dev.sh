8fd0db4ee0fc84035c3a941b73b2d9402ceb5b1a
Michal Widera <michal@widera.com.pl>
2023-08-19 21:29:09 +0200

    dev target

--- scripts/plot-dev.sh
+++ scripts/plot-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 dd if=/dev/urandom 2>/dev/null |\
-hexdump -v -e '/1 "%u 15.5\n"' |\
-plotblock.py 100 256 "data1:blue;data2:red;data3:green" --sleep 0.02 |\
+hexdump -v -e '1/1 "%u " 1/1 "%u\n"' |\
+plotblock.py 100 256 "random_1:blue;random_2:red" --sleep 0.05 |\
 gnuplot 2>/dev/null
