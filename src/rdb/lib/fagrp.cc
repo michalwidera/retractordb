5c940a366144693ed24019a905792173ba28a259
Michal Widera <michal@widera.com.pl>
2024-05-31 00:00:53 +0200

    testing

--- src/rdb/lib/fagrp.cc
+++ src/rdb/lib/fagrp.cc
@@ -13,10 +13,10 @@ groupFileAccessor<T>::groupFileAccessor(const std::string &fileName,          //
                                         const std::pair<int, int> &retention  //
                                         )
     : fileNameStr(fileName), retention(retention) {
-  if (retention == std::pair<int,int>{0,0})
+  if (retention == std::pair<int, int>{0, 0})
     vec.push_back(std::make_unique<posixBinaryFileAccessor<T>>(fileName));
   else
-    for (int segment = 0 ; segment < retention.first ; segment ++ ) {   // first == segments
+    for (int segment = 0; segment < retention.first; segment++) {  // first == segments
       std::string fname_seq = fileName + "." + std::to_string(segment);
       vec.push_back(std::make_unique<posixBinaryFileAccessor<T>>(fname_seq));
     }
@@ -32,18 +32,30 @@ std::string groupFileAccessor<T>::fileName() {
 
 template <class T>
 ssize_t groupFileAccessor<T>::write(const T *ptrData, const size_t size, const size_t position) {
-  if (retention == std::pair<int,int>{0,0})
+  if (retention == std::pair<int, int>{0, 0})
     return vec[0]->write(ptrData, size, position);
-  else
-    ; // TODO
+  else {
+    // Need to cover this with test - DOUBlECHECK
+    assert(retention.second != 0);
+    assert(retention.first != 0);
+    auto newPosition = position % retention.second;  // second == capacity
+    auto newVecIdx   = (position / retention.second) % retention.first;
+    return vec[newVecIdx]->write(ptrData, size, newPosition);
+  }
 }
 
 template <class T>
 ssize_t groupFileAccessor<T>::read(T *ptrData, const size_t size, const size_t position) {
-  if (retention == std::pair<int,int>{0,0})
+  if (retention == std::pair<int, int>{0, 0})
     return vec[0]->read(ptrData, size, position);
-  else
-    ; // TODO
+  else {
+    // Need to cover this with test - DOUBlECHECK
+    assert(retention.second != 0);
+    assert(retention.first != 0);
+    auto newPosition = position % retention.second;  // second == capacity
+    auto newVecIdx   = (position / retention.second) % retention.first;
+    return vec[newVecIdx]->read(ptrData, size, newPosition);
+  }
 }
 
 template class groupFileAccessor<uint8_t>;
