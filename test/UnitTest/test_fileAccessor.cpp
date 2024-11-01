9ce3d52b3ceee243e3a39477d6b07da5eb7e7f6c
Michal Widera <michal@widera.com.pl>
2024-11-01 10:31:37 +0100

    Work on UT

---
+++ test/UnitTest/test_fileAccessor.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+
+#include <rdb/fagrp.h>
+#include <string>
+
+TEST(fileAccessor, test_dir) {
+  ASSERT_TRUE(true);
+
+  struct {
+    char data;
+  } record ;
+
+  std::string filename = "/tmp/test_file";
+  auto recsize = sizeof(char);
+  auto silos_count = 3;
+  auto silos_size = 3;
+  auto retention = std::pair<int, int>(silos_count,silos_size);
+  auto gfa = std::make_unique<rdb::groupFileAccessor<uint8_t>>(filename,recsize,retention);
+  record.data = 1;
+  gfa->write(reinterpret_cast<uint8_t *>(&record));
+  record.data = 2;
+  gfa->write(reinterpret_cast<uint8_t *>(&record));
+}
