b72f2d78fb0ac7e68e27ca45115df5b80d9402a7
Michal Widera <michal@widera.com.pl>
2024-12-07 10:17:15 +0100

    Work on test

--- test/UnitTest/test_fileAccessor.cpp
+++ test/UnitTest/test_fileAccessor.cpp
@@ -3,14 +3,32 @@
 #include <rdb/fagrp.h>
 #include <string>
 
+#include <filesystem>
+
+const std::string sandBoxFolder = "/tmp/test_sandbox";
+
 TEST(xFileAccessor, test_dir) {
+
+  bool filepathExists = std::filesystem::is_directory(sandBoxFolder);
+
+  if (!filepathExists) {
+    std::filesystem::create_directories(sandBoxFolder);
+    std::filesystem::permissions(
+        sandBoxFolder,
+        std::filesystem::perms::others_all,
+        std::filesystem::perm_options::remove
+    );
+  }
+
+  std::filesystem::current_path(sandBoxFolder);
+
   ASSERT_TRUE(true);
 
   struct {
     char data;
   } record ;
 
-  std::string filename = "/tmp/test_file";
+  const std::string filename = "test_file";
   auto recsize = sizeof(char);
   auto silos_count = 3;
   auto silos_size = 3;
