cda0b0c6fa4fac8da344754e0ebcd4dac4575b1a
Michal Widera <michal@widera.com.pl>
2026-04-10 20:25:33 +0200

    hang on pkill fix

--- test/Data/test-all-operators.sh
+++ test/Data/test-all-operators.sh
@@ -21,8 +21,3 @@ xqry -s str3 -m 3
 xqry -s str2 -m 3
 xqry -l
 xqry -k
-
-pkill xretractor ; true
-# Ensure that the script exits cleanly even if pkill fails
-# This is useful in CI environments where pkill might not find the process
-# and return a non-zero exit code.
