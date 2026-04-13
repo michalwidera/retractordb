5db2f45b69849af1885b8fcc97b3b8839cab9a8a
Michal Widera <michal@widera.com.pl>
2026-04-13 17:31:06 +0200

    poprawka wymagan

--- src/include/rdb/fainterface.hpp
+++ src/include/rdb/fainterface.hpp
@@ -10,16 +10,15 @@ namespace rdb {
 /// @brief To jest klasa abstrakcyjna, która definiuje interfejs do operacji na różnych typach magazynów danych (np. pliki,
 /// obiekty, blob) używanych jako storage w klasie storage.
 ///
-/// obiekt klasy dziedziczącej po klasie FileInterface powinien:
-/// - zapewniać jednolity interfejs do operacji na różnych typach magazynów danych (np. pliki, obiekty, blob) używanych jako storage w klasie storage.
-/// - umożliwiać odczyt danych z magazynu na podstawie pozycji (w bajtach) i rozmiaru danych określonego przez descriptor klasy pochodnej.
-/// - w przyadku źródła danych sekwencyjnych (np. sterowników urządzeń), które nie obsługują odczytu z określonej pozycji dane odczytywane są jedynie z pozycji 0.
-/// - umożliwiać dodawanie danych na końcu magazynu, traktując określoną wartość pozycji jako sygnał do operacji append.
-/// - zapewniać informacje o liczbie rekordów w magazynie, co jest istotne dla zarządzania danymi w storage.
+/// obiekt klasy dziedziczącej po klasie FileInterface powinien zapewniać interfejs:
+/// - jednolity, do operacji na różnych typach magazynów danych (np. pliki, obiekty, blob) używanych jako storage w klasie storage.
+/// - umożliwiający odczyt danych z magazynu na podstawie pozycji (w bajtach) i rozmiaru danych określonego przez descriptor klasy pochodnej.
+/// - który, w przypadku źródła danych sekwencyjnych (np. sterowników urządzeń), które nie obsługują odczytu z określonej pozycji dane odczytywane są jedynie z pozycji 0.
+/// - umożliwiający dodawanie danych na końcu magazynu, traktując określoną wartość pozycji jako sygnał do operacji append.
+/// - dostarczający informacje o liczbie rekordów w magazynie, co jest istotne dla zarządzania danymi w storage.
 /// - w przypadku źródła danych sekwencyjnych (np. sterowników urządzeń), które nie obsługują odczytu z określonej pozycji, metoda count() może zwracać liczbę odczytów wykonanych na tym źródle danych.
 ///
-/// Oto trzy podstawowe operacje, które powinny być obsługiwane przez implementację tego interfejsu:
-/// 1. read ::= read(data, pozycja) :== odczyt danych z magazynu na podstawie pozycji (w bajtach) i rozmiaru danych określonego przez descriptor klasy pochodnej.
+/// Oto trzy podstawowe operacje, które powinny być obsługiwane przez implementację tego interfejsu:/// 1. read ::= read(data, pozycja) :== odczyt danych z magazynu na podstawie pozycji (w bajtach) i rozmiaru danych określonego przez descriptor klasy pochodnej.
 /// 2. append :== write(data, maksymalna wartość size_t) :== dodawanie danych na końcu magazynu, traktując określoną wartość pozycji jako sygnał do operacji append.
 /// 3. update :== write(data, pozycja) :== aktualizacja danych w magazynie na podstawie pozycji (w bajtach) i rozmiaru danych określonego przez descriptor klasy pochodnej.
 ///
