684dba02d954bc09423025ec7fdbf136d59e2fcb
Michal Widera <michal@widera.com.pl>
2026-05-22 23:23:47 +0200

    time test improv

--- test/IntegrationTest/issue6_adhoc/xlaunch.sh
+++ test/IntegrationTest/issue6_adhoc/xlaunch.sh
@@ -12,9 +12,9 @@ if ! xretractor query-6.rql -c; then exit 1 ; fi
 
 nohup xretractor query-6.rql </dev/null >/dev/null 2>&1 &
 echo $! > xretractor.pid
-sleep 1
+while [ ! -f /tmp/xretractor_service.lock ]; do sleep 0.1; done
 xqry -a 'select * stream stradhoc from core0'
-sleep 1
+sleep 0.1
 xqry -k
 if ps -p $(cat xretractor.pid) > /dev/null 2>&1
 then
