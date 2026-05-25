51780d4d34e323d86d7a2cafb59374b4357f8a20
Michal Widera <michal@widera.com.pl>
2026-05-25 20:58:56 +0200

    simplify token function definitions

--- src/retractor/lib/token.cpp
+++ src/retractor/lib/token.cpp
@@ -20,31 +20,25 @@ boost::rational<int> token::getRI() {
 }
 
 std::string token::getStr_() {
-  if (getVT().index() == rdb::NULLTYPE)
-    return "null";
-  else if (getVT().index() == rdb::STRING)
-    return std::get<std::string>(getVT());
-  else if (getVT().index() == rdb::FLOAT)
-    return std::to_string(std::get<float>(getVT()));
-  else if (getVT().index() == rdb::DOUBLE)
-    return std::to_string(std::get<double>(getVT()));
-  else if (getVT().index() == rdb::INTEGER)
-    return std::to_string(std::get<int>(getVT()));
-  else if (getVT().index() == rdb::UINT)
-    return std::to_string(std::get<unsigned>(getVT()));
-  else if (getVT().index() == rdb::BYTE)
-    return std::to_string(unsigned(std::get<uint8_t>(getVT())));
-  else if (getVT().index() == rdb::RATIONAL) {
-    auto r = std::get<boost::rational<int>>(getVT());
-    return std::to_string(r.numerator()) + "/" + std::to_string(r.denominator());
-  } else if (getVT().index() == rdb::INTPAIR) {
-    auto r = std::get<std::pair<int, int>>(getVT());
-    return std::to_string(r.first) + "," + std::to_string(r.second);
-  } else if (getVT().index() == rdb::IDXPAIR) {
-    auto r = std::get<std::pair<std::string, int>>(getVT());
-    return r.first;
-  } else
-    return "Error";
+  return std::visit(
+      [](const auto& v) -> std::string {
+        using T = std::decay_t<decltype(v)>;
+        if constexpr (std::is_same_v<T, std::monostate>)
+          return "null";
+        else if constexpr (std::is_same_v<T, std::string>)
+          return v;
+        else if constexpr (std::is_same_v<T, uint8_t>)
+          return std::to_string(unsigned(v));
+        else if constexpr (std::is_same_v<T, boost::rational<int>>)
+          return std::to_string(v.numerator()) + "/" + std::to_string(v.denominator());
+        else if constexpr (std::is_same_v<T, std::pair<int, int>>)
+          return std::to_string(v.first) + "," + std::to_string(v.second);
+        else if constexpr (std::is_same_v<T, std::pair<std::string, int>>)
+          return v.first;
+        else
+          return std::to_string(v);
+      },
+      getVT());
 }
 
 /** Construktor set */
@@ -55,33 +49,22 @@ token::token(command_id id, rdb::descFldVT value)
       valueVT_(std::move(value)) {}
 
 std::ostream &operator<<(std::ostream &os, const rdb::descFldVT &rhs) {
-  switch (rhs.index()) {
-    case rdb::NULLTYPE:
-      return os << "null";
-    case rdb::STRING:
-      return os << std::get<std::string>(rhs);
-    case rdb::FLOAT:
-      return os << std::get<float>(rhs);
-    case rdb::DOUBLE:
-      return os << std::get<double>(rhs);
-    case rdb::INTEGER:
-      return os << std::get<int>(rhs);
-    case rdb::UINT:
-      return os << std::get<unsigned>(rhs);
-    case rdb::BYTE:
-      return os << unsigned(std::get<uint8_t>(rhs));
-    case rdb::RATIONAL:
-      return os << std::get<boost::rational<int>>(rhs);
-    case rdb::INTPAIR: {
-      auto r = get<std::pair<int, int>>(rhs);
-      return os << r.first << "," << r.second;
-    }
-    case rdb::IDXPAIR: {
-      auto r = get<std::pair<std::string, int>>(rhs);
-      return os << r.first << "[" << r.second << "]";
-    }
-  }
-  return os << "not supported";
+  std::visit(
+      [&os](const auto& v) {
+        using T = std::decay_t<decltype(v)>;
+        if constexpr (std::is_same_v<T, std::monostate>)
+          os << "null";
+        else if constexpr (std::is_same_v<T, uint8_t>)
+          os << unsigned(v);
+        else if constexpr (std::is_same_v<T, std::pair<int, int>>)
+          os << v.first << "," << v.second;
+        else if constexpr (std::is_same_v<T, std::pair<std::string, int>>)
+          os << v.first << "[" << v.second << "]";
+        else
+          os << v;
+      },
+      rhs);
+  return os;
 }
 
 std::ostream &operator<<(std::ostream &os, const token &rhs) {
